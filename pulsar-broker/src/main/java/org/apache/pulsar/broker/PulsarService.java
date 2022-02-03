f0b9919e1e91f6d8a6e703373683ae5aec281220
aparajita.singh <aparajita.singh@flipkart.com>
2022-02-03 12:28:49 +0530

    making schema registry configurable

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/PulsarService.java
@@ -666,7 +666,7 @@ public class PulsarService implements AutoCloseable, ShutdownService {
             this.startNamespaceService();
 
             schemaStorage = createAndStartSchemaStorage();
-            schemaRegistryService = SchemaRegistryService.create(
+            schemaRegistryService = SchemaRegistryService.create(config.getSchemaRegistryClassName(),
                     schemaStorage, config.getSchemaRegistryCompatibilityCheckers());
 
             this.defaultOffloader = createManagedLedgerOffloader(
